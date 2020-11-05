662d99e3f852f660dea8a03390aeabdda297bb0c
ideaspaper <agni.infinity@gmail.com>
2020-11-05 15:24:54 +0700

    refactor: split data from script

---
+++ data.js
@@ -0,0 +1,91 @@
+const crosswordsTitle = 'Crosswords - eps. 2';
+const data = [
+  {
+    clue: 'When declaring a function, you can include ___ to make the function to expect some values when being called.',
+    answer: 'parameters',
+    x: 6,
+    y: 1,
+    orientation: 'across',
+    legend: 1
+  },
+  {
+    clue: 'Using ___ will make your code more reusable.',
+    answer: 'functions',
+    x: 1,
+    y: 6,
+    orientation: 'across',
+    legend: 5
+  },
+  {
+    clue: 'A group of the same things.',
+    answer: 'array',
+    x: 15,
+    y: 8,
+    orientation: 'across',
+    legend: 9
+  },
+  {
+    clue: 'typeof Array.',
+    answer: 'object',
+    x: 3,
+    y: 9,
+    orientation: 'across',
+    legend: 10
+  },
+  {
+    clue: 'One of the differences between String and Array.',
+    answer: 'immutability',
+    x: 6,
+    y: 11,
+    orientation: 'across',
+    legend: 11
+  },
+  {
+    clue: 'The result after calling a function.',
+    answer: 'return',
+    x: 8,
+    y: 1,
+    orientation: 'down',
+    legend: 2
+  },
+  {
+    clue: 'return statement will ___ the execution of a function.',
+    answer: 'end',
+    x: 13,
+    y: 1,
+    orientation: 'down',
+    legend: 3
+  },
+  {
+    clue: 'Best practice when naming an Array.',
+    answer: 'plural',
+    x: 18,
+    y: 4,
+    orientation: 'down',
+    legend: 4
+  },
+  {
+    clue: 'Just as with String, you can do ___ on Array.',
+    answer: 'indexing',
+    x: 6,
+    y: 6,
+    orientation: 'down',
+    legend: 6
+  },
+  {
+    clue: 'Use ___ to detect whether an Array is empty or not.',
+    answer: 'length',
+    x: 10,
+    y: 7,
+    orientation: 'down',
+    legend: 7
+  },
+  {
+    clue: 'Banana, Orange, Grapes, Apple, Kiwi.',
+    answer: 'fruits',
+    x: 16,
+    y: 7,
+    orientation: 'down',
+    legend: 8
+  },
+];
